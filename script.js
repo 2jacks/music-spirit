b3f89add407494b87f481731b5567c8fa30395ba
2jacks <anton.nozdrin.21@gmail.com>
2021-04-26 15:21:58 +0500

    go leaflet

--- script.js
+++ script.js
@@ -18,50 +18,50 @@ firebase
         console.log(data)
     })
 
-// require([
-//     'esri/Map',
-//     'esri/views/SceneView',
-//     'esri/geometry/Polygon',
-//     'esri/Graphic',
-//     'esri/layers/GraphicsLayer',
-//     'esri/layers/FeatureLayer',
-//     'esri/layers/GeoJSONLayer',
-// ], function (Map, SceneView, GraphicsLayer, FeatureLayer, GeoJSONLayer) {
-//     var map = new Map({
-//         basemap: 'topo-vector',
-//         ground: 'world-elevation',
-//     })
-//     var view = new SceneView({
-//         container: 'viewDiv',
-//         map: map,
-//         scale: 50000000,
-//         center: [12.411109924316406, -69.882232666015625],
-//     })
+require([
+    'esri/Map',
+    'esri/views/SceneView',
+    'esri/geometry/Polygon',
+    'esri/Graphic',
+    'esri/layers/GraphicsLayer',
+    'esri/layers/FeatureLayer',
+    'esri/layers/GeoJSONLayer',
+], function (Map, SceneView, GraphicsLayer, FeatureLayer, GeoJSONLayer) {
+    var map = new Map({
+        basemap: 'topo-vector',
+        ground: 'world-elevation',
+    })
+    var view = new SceneView({
+        container: 'viewDiv',
+        map: map,
+        scale: 50000000,
+        center: [12.411109924316406, -69.882232666015625],
+    })
 
-//     const graphicsLayer = new GraphicsLayer()
+    const graphicsLayer = new GraphicsLayer()
 
-//     const simpleFillSymbol = {
-//         type: 'simple-fill',
-//         color: [227, 139, 79, 0.8], // Orange, opacity 80%
-//         outline: {
-//             color: [255, 255, 255],
-//             width: 1,
-//         },
-//     }
+    const simpleFillSymbol = {
+        type: 'simple-fill',
+        color: [227, 139, 79, 0.8], // Orange, opacity 80%
+        outline: {
+            color: [255, 255, 255],
+            width: 1,
+        },
+    }
 
-//     const geoJSONLayer = new GeoJSONLayer({
-//         url:
-//             'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson',
-//         copyright: 'USGS Earthquakes',
-//     })
+    const geoJSONLayer = new GeoJSONLayer({
+        url:
+            'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson',
+        copyright: 'USGS Earthquakes',
+    })
 
-//     const parcelLayer = new FeatureLayer({
-//         url:
-//             'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/World_Countries_(Generalized)/FeatureServer/0',
-//     })
+    const parcelLayer = new FeatureLayer({
+        url:
+            'https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/World_Countries_(Generalized)/FeatureServer/0',
+    })
 
-//     view.on('click', (e) => {
-//         console.log(e)
-//     })
-//     map.add(parcelLayer)
-// })
+    view.on('click', (e) => {
+        console.log(e)
+    })
+    map.add(parcelLayer)
+})
